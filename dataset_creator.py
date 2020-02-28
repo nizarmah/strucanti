543722dd1e7b2d00a8f368bd923dc54c5a9e70e7
Nizar Mahmoud <nizar.mah99@gmail.com>
2020-02-28 18:43:54 +0200

    Feature: Parallelized the Dataset Creation by Assigning Number of Processes

--- dataset_creator.py
+++ dataset_creator.py
@@ -6,6 +6,72 @@ from os import path
 import numpy as np
 import pandas as pd
 
+from functools import partial
+from multiprocessing import Pool, cpu_count
+
+class HCADB(object):
+	"""
+	Creates HCADB object out of HCADB CSV Path
+	Also creates a Dictionary Mapping Key -> Value
+		- Key   : Encoded Representation of Protein 
+		- Value : Protein Name
+
+	> Note : the Value represents the protein column name in the dataset
+			 it is formed through concatting Protein Code and Affinity
+	"""
+	def __init__(self, hcadb_path):
+		self.df = pd.read_csv(hcadb_path)
+
+		self.protein_names = (self.df['P-code'].astype(str)
+								+ '_' + self.df['affinity']).values
+
+		self.encoded_proteins = self.df['Binary'].astype(str).values
+
+		self.proteins_dict = dict(zip(self.encoded_proteins, 
+										self.protein_names))
+
+	def get_protein_name(self, binary_seq):
+		if binary_seq in self.proteins_dict:
+			return self.proteins_dict[binary_seq]
+
+		return None
+
+	def protein_names_empty_dataset(self, num_rows):
+		"""
+		Creates a DataFrame with Columns as Protein Name filled with 0s
+		"""
+		return pd.DataFrame(np.zeros((num_rows, len(self.protein_names))), 
+								columns=self.protein_names)
+
+class SCOP(object):
+	"""
+	Creates SCOP object out of SCOP CSV Path
+	Also creates a DataFrame of all Sequences in the SCOP CSV file
+	"""
+	def __init__(self, scop_path):
+		self.df = pd.read_csv(scop_path)
+
+		self.num_rows = self.df.shape[0]
+
+	def join(self, other_df):
+		self.df = self.df.join(other_df)
+
+	def distribute(self, func, num_processes):
+		self.num_processes = num_processes
+
+		self.chunksize = (self.num_rows // self.num_processes)
+
+		with Pool(self.num_processes) as pool:
+			return pool.map(func, range(num_processes))
+
+	def get_segment(self, process_id):
+		starting_index = (self.chunksize * process_id)
+		next_starting_index = (self.chunksize * (process_id + 1))
+
+		return self.df.iloc[starting_index:] \
+				if ((process_id + 1) == self.num_processes) \
+					else self.df.iloc[starting_index:next_starting_index]
+
 class AminoEncoder(object):
 	"""
 	Encodes certain amino acids as 1,
@@ -37,20 +103,8 @@ class EncodedSequenceEnumerator(object):
 	The series is used in order to find ProteinCode_Affinity
 	According to the Binary Sequences extracted from the Encoded Sequence
 	"""
-	def __init__(self, binary_protein_series):
-		self.binary_protein_series = binary_protein_series
-
-	def find_record(self, binary_seq):
-		"""
-		Searches (ProteinCode_Affinity : Binary Sequence) Series
-		For certain Binary Sequence, in order to retrieve ProteinCode_Affinity
-		"""
-		record = self.binary_protein_series[self.binary_protein_series
-															== binary_seq]
-
-		if len(record) == 1:
-			return (record.index[0], record.iloc[0])
-		else: return None
+	def __init__(self, hcadb):
+		self.hcadb = hcadb
 
 	def enumerate(self, row):
 		"""
@@ -59,59 +113,74 @@ class EncodedSequenceEnumerator(object):
 		binary_seqs = re.split('(0*p0*)|(0{4,})', row['encoded_sequence'])
 
 		for binary_seq in binary_seqs:
-			protein_record = self.find_record(binary_seq)
+			protein_name = self.hcadb.get_protein_name(binary_seq)
 
-			if protein_record != None:
-				row[protein_record[0]] += 1
+			if protein_name != None:
+				row[protein_name] += 1
 
 		return row
 
+def enumerate_sequences(hcadb, scop, process_id):
+	"""
+	Takes a segment of the DataFrame and counts proteins
+	in sequences after filtering the sequences and encoding them
+	"""
+	df = scop.get_segment(process_id)
 
-def create_dataset(hcadb_path, scop_path):
+	amino_encoder = AminoEncoder()
+
+	# drop all entries that contain 'x' in their sequence
+	df = df[[ ('x' not in i) for i in df.sequence ]]
+
+	# encode sequences into a new series
+	df['encoded_sequence'] = df.sequence.map(amino_encoder.encode)
+
+	encoded_seq_enumerator = EncodedSequenceEnumerator(hcadb)
+	df = df.apply(encoded_seq_enumerator.enumerate, axis='columns')
+
+	return df.drop('encoded_sequence', 1)
+
+def create_dataset(hcadb_path, scop_path, num_processes=1):
 	"""
 	Creates DataSet from 
 	- Dictionary Path
 	- SCOPe Sequence Set Path
 	"""
-	scop_df = pd.read_csv(scop_path)
-	hcadb_df = pd.read_csv(hcadb_path)
+	hcadb = HCADB(hcadb_path)
+	
+	scop = SCOP(scop_path)
+	scop.join(hcadb.protein_names_empty_dataset(scop.num_rows))
 
-	amino_encoder = AminoEncoder()
+	dataset_distributed = scop.distribute(
+								partial(enumerate_sequences, hcadb, scop), 
+								num_processes)
 
-	# drop all entries that contain 'x' in their sequence
-	scop_df = scop_df[[ ('x' not in i) for i in scop_df.sequence ]]
-
-	# encode sequences into a new series
-	encoded_seqs_series = scop_df.sequence.map(amino_encoder.encode) \
-											.rename('encoded_sequence')
-
-	# get all protein codes and affinities
-	protein_codes_series = (hcadb_df['P-code'].astype(str)
-							+ '_' + hcadb_df['affinity'])
-
-	# combine protein_codes_series with their binary protein sequences
-	binary_protein_series = hcadb_df['Binary'].astype(str)
-	binary_protein_series.index = protein_codes_series.values
-
-	protein_codes_df = binary_protein_series.to_frame().T[0:0]
-	encoded_seqset_df = pd.concat([encoded_seqs_series, 
-									protein_codes_df], axis=1)
-	encoded_seqset_df.fillna(0, inplace=True)
-
-	encoded_seq_enumerator = EncodedSequenceEnumerator(binary_protein_series)
-	encoded_dataset_df = encoded_seqset_df.apply(
-							encoded_seq_enumerator.enumerate, axis='columns')
-
-	dataset_df = pd.concat([scop_df, 
-								encoded_dataset_df], axis=1)
+	dataset_df = pd.concat(dataset_distributed)
 
 	scop_filename = path.splitext(path.basename(scop_path))[0]
 	dataset_file = 'dataset_' + scop_filename + '.csv'
 	dataset_df.to_csv(dataset_file, index=False)
 
+def start_script():
+	"""
+	Reads Arguments from CMD
+	And Starts the Script Accordingly
+	"""
+	num_processes = cpu_count() - 1
+	hcadb_path = sys.argv[1]
+	scop_path = sys.argv[2]
+
+	if sys.argv[1] == '--processes':
+		num_processes = sys.argv[2]
+
+		hcadb_path = sys.argv[3]
+		scop_path = sys.argv[4]
+
+	create_dataset(hcadb_path, scop_path, num_processes=num_processes)
+
 if __name__ == '__main__':
 	try:
-		create_dataset(sys.argv[1], sys.argv[2])
+		start_script()
 	except:
 		print('Error : Missing Arguments')
 		print('Usage : python ', path.basename(__file__), 
